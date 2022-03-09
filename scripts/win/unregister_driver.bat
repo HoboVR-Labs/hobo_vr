c9f574151f81cdb0c8016036203ed599ff1a15f9
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-03-09 21:23:20 +0200

    scripts: win: Fixed an even dumber bug
    Windows batch files... complete garbage!
    
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

--- scripts/win/unregister_driver.bat
+++ scripts/win/unregister_driver.bat
@@ -1,6 +1,6 @@
 @echo off
 IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
-	SET var="%~dp0"
+	SET "var=%~dp0"
 
 	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver %var%
 	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
