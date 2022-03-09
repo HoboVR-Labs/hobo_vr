e92ede8cbda522a94d09474a0c28cec13abe5508
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-03-09 19:43:41 +0200

    scripts: win: Fixed win driver scripts
    Fixed windows scripts, now they actually work...
    
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

--- scripts/win/register_driver.bat
+++ scripts/win/register_driver.bat
@@ -1,21 +1,13 @@
 @echo off
-rem IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
-rem IF EXIST "%~dp0..\..\hobovr" (
-rem 	set old_pwd="%~dp0"
-rem 	cd  "..\..\hobovr"
+IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
+rem fuck windows, this shit is disgusting, you need a fucking for loop to capture an output of a command wtf
+	FOR /F "tokens=* USEBACKQ" %%F IN (`cd`) DO (
+	SET var=%%F
+	)
+	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver "%var%"
+	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
 
-rem rem fuck windows, this shit is disgusting, you need a fucking for loop to capture an output of a command wtf
-rem 	FOR /F "tokens=* USEBACKQ" %%F IN (`cd`) DO (
-rem 	SET var=%%F
-rem 	)
-rem 	cd %old_pwd%
-rem 	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver "%var%"
-rem 	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
-rem ) ELSE (
-rem 	echo "could not find driver, is the repository broken?"
-rem )
-rem ) ELSE (
-rem echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed"
-rem )
-echo "this script is not finished..."
+) ELSE (
+	echo "SteamVR not located in C:\\Program Files (x86)\\Steam\\steamapps\\common\\SteamVR - Installation Failed"
+)
 pause
