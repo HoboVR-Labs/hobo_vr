50cc42252ea8f539ac2febdf9ce43e80f15c0c38
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-03-09 20:53:38 +0200

    scripts: win: Fixed a really dumb path bug
    Fixed a really dumbpath bug... only on windows!
    
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

--- scripts/win/register_driver.bat
+++ scripts/win/register_driver.bat
@@ -1,10 +1,8 @@
 @echo off
 IF EXIST "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\" (
-rem fuck windows, this shit is disgusting, you need a fucking for loop to capture an output of a command wtf
-	FOR /F "tokens=* USEBACKQ" %%F IN (`cd`) DO (
-	SET var=%%F
-	)
-	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver "%var%"
+	SET var="%~dp0"
+
+	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver %var%
 	"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" show
 
 ) ELSE (
