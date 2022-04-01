91042ef0cb522d0178a49812e2c1cb0fe1fa686e
Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
2022-04-02 01:26:36 +0300

    driver: src: Added create device factory
    Added a create device factory for simpler device management.
    
    Signed-off-by: Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>

---
+++ driver/src/device_factory.cpp
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Copyright (C) 2020-2021 Oleg Vorobiov <oleg.vorobiov@hobovrlabs.org>
+
+#include "device_factory.h"
+
+#include <openvr_driver.h>
+#include <memory>
+
+#include "driverlog.h"
+
+// devices
+#include "headsets.h"
+#include "controllers.h"
+#include "trackers.h"
+#include "addons.h"
+
+
+std::unique_ptr<hobovr::IHobovrDevice> hobovr::createDeviceFactory(
+	char type,
+	int number,
+	std::shared_ptr<hobovr::tcp_socket> soc
+) {
+
+	switch(type) {
+		case 'h': {
+			DriverLog("%s: Creating Headset device: h%d", __FUNCTION__, number);
+
+			auto tmp = std::make_unique<HeadsetDriver>(type + std::to_string(number));
+
+			vr::VRServerDriverHost()->TrackedDeviceAdded(
+				tmp->GetSerialNumber().c_str(),
+				vr::TrackedDeviceClass_HMD,
+				tmp.get()
+			);
+
+			return tmp;
+		}
+
+		case 'c': {
+			DriverLog("%s: Creating Controller device: c%d", __FUNCTION__, number);
+
+			auto tmp = std::make_unique<ControllerDriver>(
+				!!(number % 2),
+				type + std::to_string(number),
+				soc
+			);
+
+			vr::VRServerDriverHost()->TrackedDeviceAdded(
+				tmp->GetSerialNumber().c_str(),
+				vr::TrackedDeviceClass_Controller,
+				tmp.get()
+			);
+
+			return tmp;	
+		}
+
+		case 't': {
+			DriverLog("%s: Creating Tracker device: t%d", __FUNCTION__, number);
+			
+			auto tmp = std::make_unique<TrackerDriver>(
+				type + std::to_string(number),
+				soc
+			);
+
+			vr::VRServerDriverHost()->TrackedDeviceAdded(
+				tmp->GetSerialNumber().c_str(),
+				vr::TrackedDeviceClass_GenericTracker,
+				tmp.get()
+			);
+
+			return tmp;
+		}
+
+		case 'g': {
+			DriverLog("%s: Creating GazeMaster device: t%d", __FUNCTION__, number);
+
+			auto tmp = std::make_unique<GazeMasterDriver>(type + std::to_string(number));
+
+			vr::VRServerDriverHost()->TrackedDeviceAdded(
+				tmp->GetSerialNumber().c_str(),
+				vr::TrackedDeviceClass_GenericTracker,
+				tmp.get()
+			);
+
+			return tmp;
+		}
+
+		default:
+			DriverLog("%s: bad decive type: %c", __FUNCTION__, type);
+	}
+
+	return nullptr;
+}
